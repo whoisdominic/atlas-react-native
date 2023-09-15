74cfa070f97ea35678a71342b8b86015f73728dd
Dominic <dxc3986@yum.com>
2023-09-14 17:03:53 -0700

    docs added for guards

--- src/hooks/guards/types.ts
+++ src/hooks/guards/types.ts
@@ -1,4 +1,29 @@
-interface GuardBase {
+/**
+ * `GuardConfig` is the configuration object for the `useGuard` hook. It allows
+ * you to specify the conditions under which an action should be executed.
+ * @property {boolean} condition - Determines whether the action should be executed.
+ * The action is executed when this property is set to `true`.
+ *
+ * @property {Function} action - The function that will be executed when the
+ * condition is met. It is recommended to use a function that returns `void`.
+ *
+ * @property {boolean} [disabled=false] - When set to `true`, the guard action is
+ * disabled, and the action will not be executed, regardless of the condition property's value.
+ *
+ * @property {boolean} [refireOnFocus=false] - When set to `true`, the action will
+ * be executed again every time the screen regains focus, as long as the condition is `true`.
+ * @example
+ * {
+ *  condition: true,
+ *  action: () => console.log('Hello World'),
+ *  disabled: false,
+ *  refireOnFocus: false,
+ * }
+ *
+ * @since 1.0.0
+ * @version 1.0.0
+ */
+interface GuardConfig {
   condition: boolean; // Determines if the action should be executed
   action: () => void; // The action to execute
   disabled?: boolean; // Disable the guard action
--- src/hooks/guards/useGuard.ts
+++ src/hooks/guards/useGuard.ts
@@ -1,12 +1,28 @@
 import { useIsFocused } from "@react-navigation/native";
 import { useCallback, useEffect, useRef } from "react";
 
+/**
+ * `useGuard` is a React hook that conditionally executes a specified action.
+ * The action is executed when the specified condition is true. The action execution
+ * can be controlled further using the `disabled` and `refireOnFocus` options.
+ *
+ * @param params - The parameters for the useGuard hook.
+ *
+ * @example
+ * // Executes the action if the condition is true
+ * useGuard({ condition: true, action: () => console.log('Condition met') });
+ *
+ * // Executes the action if the condition is true and the screen is focused
+ * useGuard({ condition: true, action: () => console.log('Condition met'), refireOnFocus: true });
+ *
+ * @since 1.0.0
+ */
 export function useGuard({
   condition,
   action,
   disabled,
   refireOnFocus,
-}: GuardBase) {
+}: GuardConfig) {
   const focused = useIsFocused();
 
   const prevConditionRef = useRef(condition);
